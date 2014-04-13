37b4ab2790c2469fdbd7cefbdd5e63aecfbf8758
prototype <lyubomir@acewebdesign.com.au>
2014-04-13 22:26:28 +0300

    check to work only for the current module

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -19,6 +19,9 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        
+        $eventManager->attach(MvcEvent::EVENT_ROUTE,
+        		array($this, 'protectPage'), -100);
     }
 
     public function getConfig()
@@ -35,5 +38,35 @@ class Module
                 ),
             ),
         );
+    }
+    
+    public function protectPage(MvcEvent $event)
+    {
+    	$match = $event->getRouteMatch();
+    	if(!$match) {
+    		// we cannot do anything without a resolved route
+    		return;
+    	}
+    	$controller = $match->getParam('controller');
+    	$action = $match->getParam('action');
+    	$namespace = $match->getParam('__NAMESPACE__');
+    	
+    	//check to work only for the current module
+    	if (strpos($namespace,__NAMESPACE__)!==0) {
+    	
+    		return;
+    	}
+    	 
+    	$services = $event->getApplication()->getServiceManager();
+    	 
+    	$auth = $services->get('auth');
+    	if (!$auth->hasIdentity()) {
+    		// Set the response code to HTTP 401: Auth Required
+    		$response = $event->getResponse();
+    		$response->setStatusCode(401);
+    
+    		$match->setParam('controller', 'Application\Controller\Users');
+    		$match->setParam('action', 'login');
+    	}
     }
 }
---
+++ module/Application/src/Application/Controller/AccountController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class AccountController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+    public function meAction()
+    {
+        return array();
+    }
+
+    public function deniedAction()
+    {
+        return array();
+    }
+}
