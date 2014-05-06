ce50ba566ea2affc5b07626518720dece023cd2e
prototype <lyubomir@acewebdesign.com.au>
2014-05-07 00:40:14 +0300

    add form elements

--- module/Application/src/Application/Form/RegisterParticipant.php
+++ module/Application/src/Application/Form/RegisterParticipant.php
@@ -15,15 +15,96 @@ class RegisterParticipant extends Form implements ObjectManagerAwareInterface
 	
 		parent::__construct('db-adapter-form');
 	
+		
+		
+		//$this->setAction($this->url(array(), 'default'));
+		//$this->setMethod('post');
+		
+		$this->add(array(
+				'name' => 'names',
+				'options' => array(
+						'label' => 'Име, презиме, фамилия',
+				),
+				'attributes' => array(
+						'type'  => 'text',
+				),
+		));
+		
 		$this->add(array(
 				'type'    => 'DoctrineModule\Form\Element\ObjectSelect',
-				'name'    => 'name',
+				'name'    => 'area',
 				'options' => array(
-						'label'          => 'Dynamic ObjectManager Select',
+						'label'          => 'Област',
 						'object_manager' => $this->getObjectManager(),
 						'target_class'   => 'Application\Model\Entity\Areas',
 						'property'       => 'areaname',
-						'empty_option'   => '--- please choose ---'
+						'empty_option'   => 'Изберете област'
+				),
+		));
+		
+		$this->add(array(
+				'type' => 'Zend\Form\Element\Select',
+				'name' => 'city',
+				'options' => array(
+						'label' => 'Населено място',
+						'value_options' => array(
+								'0' => 'Изберете населено място'
+						),
+				)
+		)); 
+		
+		$this->add(array(
+				'type' => 'Zend\Form\Element\Checkbox',
+				'name' => 'anothercity',
+				'options' => array(
+						'label' => 'Друго населено място',
+						'use_hidden_element' => true,
+						'checked_value' => 'good',
+						'unchecked_value' => 'bad'
+				)
+		));
+		
+		$this->add(array(
+				'type' => 'Zend\Form\Element\Select',
+				'name' => 'school',
+				'options' => array(
+						'label' => 'Училище',
+						'value_options' => array(
+								'0' => 'Изберете училище'
+						),
+				)
+		));
+		
+		$this->add(array(
+				'type' => 'Zend\Form\Element\Checkbox',
+				'name' => 'another',
+				'options' => array(
+						'label' => 'Друго училище',
+						'use_hidden_element' => true,
+						'checked_value' => 'good',
+						'unchecked_value' => 'bad'
+				)
+		));
+		
+		$this->add(array(
+				'type' => 'Zend\Form\Element\Select',
+				'name' => 'grade',
+				'options' => array(
+						'label' => 'Клас',
+						'value_options' => array(
+								'11'=>'11',
+								'12'=>'12'
+						),
+				)
+		));
+		
+		$this->add(array(
+				'name' => 'address',
+				'options' => array(
+						'label' => 'Адрес',
+				),
+				'attributes' => array(
+						'type'  => 'text',
 				),
 		));
 	}
